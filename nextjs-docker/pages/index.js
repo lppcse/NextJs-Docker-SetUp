6d7ea3164704f95d94bb28ce4edec8aa2b1b0bc2
lppcse <lokeshcse1990@gmail.com>
2023-09-11 02:03:09 -0500

    steps to be followed - docker image, container to gcloud deployement

--- nextjs-docker/pages/index.js
+++ nextjs-docker/pages/index.js
@@ -12,6 +12,7 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js</a> on Docker!
+          <span>by lppcse</span>
         </h1>
 
         <p className={styles.description}>
